39dd3452c11d1f224c3b6ab93ce8e0febf45329e
jackdamnielzz <maassure@gmail.com>
2024-12-09 12:37:18 +0100

    06

---
+++ httpdocs/js/contact-form-docs.js
@@ -0,0 +1,18 @@
+/**
+ * 🚨 KRITIEKE DOCUMENTATIE - CONTACT FORMULIER IMPLEMENTATIE 🚨
+ *
+ * WAARSCHUWING: DIT DOCUMENT BEVAT ESSENTIËLE TECHNISCHE RICHTLIJNEN
+ *
+ * 1. TECHNISCHE INTEGRITEIT
+ * -------------------------
+ * - JavaScript MOET inline in contact.html blijven
+ * - NOOIT externe JavaScript bestanden gebruiken voor form handling
+ * - Alle console.log statements zijn CRUCIAAL voor debugging
+ * - Oorspronkelijke form ID 'contact-experiment-form' MOET behouden blijven
+ * - Feedback div ID MOET 'form-feedback' blijven
+ *
+ * 2. VALIDATIE PRINCIPES
+ * ----------------------
+ * - ALLE verplichte velden (naam, email, onderwerp, bericht) MOETEN gecontroleerd worden
+ * - Submit button status wordt bepaald door validatie
+ */
