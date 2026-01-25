5558bfe5690560dab9dc6ba88fa7c8ab8a0e9e97
jackdamnielzz <maassure@gmail.com>
2026-01-25 14:52:14 +0100

    chore: cleanup redundant code-level redirects now that dashboard redirect is being configured

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,6 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const hostname = request.headers.get('host');
-  
-  // Force WWW to non-WWW in a single hop if possible
-  if (hostname === 'www.maasiso.nl') {
-    return NextResponse.redirect(
-      `https://maasiso.nl${request.nextUrl.pathname}${request.nextUrl.search}`,
-      308
-    );
-  }
-
   // Redirect /home to / to prevent duplicate content
   if (request.nextUrl.pathname === '/home') {
     return NextResponse.redirect(new URL('/', request.url), 301);
@@ -30,14 +20,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ['/sitemap.xml', '/home'],
 };
