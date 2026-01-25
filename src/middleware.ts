28cf84bcd6da1ba1119cc1a4c7bfea9b536bc56f
jackdamnielzz <maassure@gmail.com>
2026-01-25 14:50:11 +0100

    chore: add www to non-www redirect in middleware for potential 1-hop

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,16 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  const hostname = request.headers.get('host');
+  
+  // Force WWW to non-WWW in a single hop if possible
+  if (hostname === 'www.maasiso.nl') {
+    return NextResponse.redirect(
+      `https://maasiso.nl${request.nextUrl.pathname}${request.nextUrl.search}`,
+      308
+    );
+  }
+
   // Redirect /home to / to prevent duplicate content
   if (request.nextUrl.pathname === '/home') {
     return NextResponse.redirect(new URL('/', request.url), 301);
@@ -20,5 +30,14 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/sitemap.xml', '/home'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
 };
